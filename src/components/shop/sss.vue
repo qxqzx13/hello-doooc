7d21bd21e5ecbd93b32adbc61fa7c59d1440b877
lxloner <x18234115020@163.com>
2019-04-27 10:28:12 +0800

    Revert "Revert "Merge branch '；x1'""
    This reverts commit 9bc5f9a281550a5d898a59f546fa9380be94beeb.

---
+++ src/components/shop/sss.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="sss">
+        <cart></cart>
+        <gempty></gempty>
+    </div>
+</template>
+
+<script>
+    import cart from "./cart.vue";
+    import gempty from "./gempty.vue";
+    export default {
+        name: "sss",
+        components:{
+            cart,
+            gempty
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/shoppingcar.vue
+++ src/views/shoppingcar.vue
@@ -7,48 +7,8 @@
             <div class="petpic"></div>
             <h3>宠物详情</h3>
             <div class="nan">
-                <div class="details1">
-                    <h1></h1>
-                    <h2></h2>
-                    <div class="commodity">
-                        <p>商品详细</p>
-                        <p>麦富迪 双拼标准颗粒成犬粮10KG</p>
-                        <P>4:1牛肉粒配比，适口性更好！营养更丰富,
-                            全犬种成犬可用
-                        </P>
-                    </div>
-                    <ul>
-                        <li>
-                            <p>适用阶段</p>
-                            <span>幼犬</span>
-                        </li>
-                        <li>
-                            <p class="taste">口味</p>
-                            <select name="" id="">
-                                <option value="1">xl</option>
-                            </select>
-                        </li>
-                        <li>
-                            <p>生产地址</p>
-                            <span>中国</span>
-                        </li>
-                        <li>
-                            <p class="taste">重量</p>
-                            <span>2000g</span>
-                        </li>
-                        <li>
-                            <p class="taste1">保质期<i></i></p>
-                            <span>18个月</span>
-                        </li>
-                    </ul>
-                    <el-row>
-                        <el-button round>加入购物车</el-button>
-                    </el-row>
-                    <p class="price">单价<span>￥500</span></p>
-                    <p class="total">总计：<span>￥5000</span></p>
-                    <p class="quantity">数量<span>x1</span><img src="../assets/petshop/img/plus.svg" alt=""><img src="../assets/petshop/img/minus.svg"
-                                                                                                               alt=""></p>
-                </div>
+                <detail v-show = "  !isShow"></detail>
+                <sss v-show = "isShow"></sss>
             </div>
         </div>
 	</div>
@@ -59,17 +19,26 @@
 <script>
 import navigation from "../components/shopcommonality/navigation";
 import commonFooter from "../components/common/commonFooter.vue";
+import detail from "../components/shop/detail.vue";
+import sss from "../components/shop/sss.vue";
+
 import {mapState,mapMutations,mapGetters,mapActions} from "vuex";
+
 export default {
     name: 'shoppingcar',
     components: {
         commonFooter,
-        navigation
+        navigation,
+        detail,
+        sss
+    },
+    data(){
+        return {isShow:true}
     },
     methods:mapActions(["toShoppingCar"]),
-    mounted() {
+    mounted(){
         this.toShoppingCar(this.$route.query.productId);
-}
+    }
 }
 </script>
 <style scoped>
@@ -116,119 +85,6 @@ export default {
         background:#f7e2cb;
         margin-top:-106px;
         margin-left:294px;
-    }
-        
-    #middle #car .nan .details1{
-        height:515px;
-        width:730px;
-        background:#fbf1e6;
-        left:60px;
-        top:30px;
-        position:relative;
-        
-    }
-    #middle #car .nan .details1 h1{
-        width: 0;
-        border: 12px solid transparent;
-        border-right: 12px solid #f5a64a;
-        position:absolute;
-        top:186px;
-        left:24px;
-    }
-    #middle #car .nan .details1 h2{
-        height:270px;
-        width:270px;
-        background:#fff;
-        position:absolute;
-        top:76px;
-        left:64px;
-    }
-    .details1 .el-button{
-        height:22px;
-        width:108px;
-        line-height:1px;
-        background: #f99725;
-        color: #000;
-        position: absolute;
-        top: 464px;
-        right:46px;
-    }
-    .details1 .quantity{
-        position:absolute;
-        bottom:154px;
-        right:110px;
-        height:24px;
-        text-align: center;
-    }
-    .details1 .quantity img{
-        height:18px;
-        width:24px;
-        cursor: pointer;
-        vertical-align: middle;
-
-    }
-    .details1 .total{
-        position: absolute;
-        bottom:28px;
-        right:200px;
-        font-size:16px;
-        color:#f99725
-    }
-    .details1 .total span{
-        color:#000;
-    }
-    .details1 .price{
-        position: absolute;
-        bottom:154px;
-        right:274px;
-        font-size:16px;
-    }
-    .commodity{
-        position:absolute;
-        left:384px;
-        top:72px;
-        height:76px;
-        width:186px;
-    }
-    .commodity p{
-        font-size:10px;
-        line-height:24px;
-    }
-    .details1 ul{
-        height:96px;
-        width:192px;
-        position:absolute;
-        top:208px;
-        left:384px;
-        
-    }
-    .details1 ul li{
-        font-size:10px;
-    }
-    .details1 ul li p{
-        display: inline-table;
-        margin-right:12px;
-        width:48px;        
-    }
-    .details1 ul li:nth-child(odd){
-        background: #f5a64a;
-    }
-    .details1 ul li:nth-child(even){
-        line-height:30px;
-        height:30px;
-    }
-    .taste{
-        text-align: justify;
-
-    }
-    .taste:after {
-        display: inline-block;
-        width: 100%;
-        content: '';
-    }
-    .taste1{
-    text-align: justify;
-    text-align-last: justify;
     }
     
 </style>
